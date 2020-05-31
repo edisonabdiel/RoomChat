4eed607fb6f7f0ff3e0fbb42519bb698d1f19fb1
Edison Abdiel <43752457+edisonabdiel@users.noreply.github.com>
2020-05-31 18:44:58 +0200

    Create Join component Create index.js Users.js router.js (#1)

--- server/index.js
+++ server/index.js
@@ -2,10 +2,29 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const {addUser, removeUser, getUser, getUsersInRoom} = require('./users')
+
 const PORT = process.env.PORT || 5000;
+
+const router = require('./router')
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', (socket) => {
+
+    socket.on('join', ({ name, room }, callBack) => {
+        const [error, user] = addUser({ id: socket.id, name, room });
+
+        if (error) return callBack(error);
+    });
+
+    socket.on('disconnect', () => {
+        
+    })
+})
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
